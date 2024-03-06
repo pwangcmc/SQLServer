e0cb9bc33f69bf2995453368f88ef5c40648647c
talktopeng <talktopeng@gmail.com>
2024-03-06 12:37:39 +1100

    Update CheckTransInfo.sql

--- DEV/Transaction/CheckTransInfo.sql
+++ DEV/Transaction/CheckTransInfo.sql
@@ -17,6 +17,14 @@ concat_null_yields_null	      SET
 isolation level	              read committed
 */
 
-SELECT transaction_isolation_level 
+SELECT 
+  CASE transaction_isolation_level
+	  WHEN 1 THEN 'ReadUncomitted'
+	  WHEN 2 THEN 'ReadCommitted'
+	  WHEN 3 THEN 'Repeatable'
+	  WHEN 4 THEN 'Serializable'
+	  WHEN 5 THEN 'Snapshot'
+    ELSE 'Unspecified' 
+  END AS transaction_isolation_level,
 FROM sys.dm_exec_sessions 
 WHERE session_id = @@SPID
