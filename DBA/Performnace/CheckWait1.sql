757927083809c08717174cd1406956b84980f751
talktopeng <talktopeng@gmail.com>
2024-03-10 20:47:16 +1100

    Update CheckWait1.sql

--- DBA/Performnace/CheckWait1.sql
+++ DBA/Performnace/CheckWait1.sql
@@ -14,3 +14,11 @@ SELECT
 	, wait_duration_ms / 1000 / 60.0 DurationInMins
 FROM sys.dm_os_waiting_tasks
 WHERE session_id = 51
+
+SELECT  
+	session_id  BlockedSPID -- which run the select statement
+	, blocking_session_id BlockedBy  -- which run the truncate table 
+	, wait_type -- LCK_M_SCH_S
+	, wait_duration_ms / 1000 / 60.0 DurationInMins
+FROM sys.dm_os_waiting_tasks
+WHERE blocking_session_id IS NOT NULL
