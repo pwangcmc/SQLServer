e14bc5dc06341bf59f26c2d6bef5a5b83fe710b7
talktopeng <talktopeng@gmail.com>
2024-11-20 11:28:11 +1100

    Update Coalesce_NULL.sql

--- Dev/TK/Coalesce_NULL.sql
+++ Dev/TK/Coalesce_NULL.sql
@@ -2,20 +2,22 @@
 The COALESCE function in SQL is used to return the first non-null value in a list of expressions. Let's break down the expression you provided:
 
 COALESCE([a].[DojoBusinessRegionID], 1) = 1
-[a].[DojoBusinessRegionID] refers to a column (likely an alias a is used for a table).
-The COALESCE function checks if [a].[DojoBusinessRegionID] is null:
-If not null, it returns the value of [a].[DojoBusinessRegionID].
-If null, it returns 1 (the default value provided as the second argument in COALESCE).
+
+* [a].[DojoBusinessRegionID] refers to a column (likely an alias a is used for a table).
+* The COALESCE function checks if [a].[DojoBusinessRegionID] is null:
+    * If not null, it returns the value of [a].[DojoBusinessRegionID].
+    * If null, it returns 1 (the default value provided as the second argument in COALESCE).
 Comparison: = 1
-The entire expression is then compared to 1.
-If [a].[DojoBusinessRegionID] is null, COALESCE will return 1, so the expression COALESCE([a].[DojoBusinessRegionID], 1) = 1 evaluates to true.
-If [a].[DojoBusinessRegionID] has a value other than null, the expression evaluates based on whether that value is equal to 1.
+* The entire expression is then compared to 1.
+* If [a].[DojoBusinessRegionID] is null, COALESCE will return 1, so the expression COALESCE([a].[DojoBusinessRegionID], 1) = 1 evaluates to true.
+* If [a].[DojoBusinessRegionID] has a value other than null, the expression evaluates based on whether that value is equal to 1.
+
 In summary:
 This code checks if the column [a].[DojoBusinessRegionID] is either null (in which case the comparison will be true) or equal to 1.
 
 */
 
-Same equivalent
+-- Same equivalent
 CASE 
     WHEN [a].[DojoBusinessRegionID] IS NULL THEN 1
     ELSE [a].[DojoBusinessRegionID]
